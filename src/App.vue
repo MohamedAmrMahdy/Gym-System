0bfe02b4fb6b6ab8dd3862deeba7c5545447386d
Mohamed Amr Mahdy <dev.mohamed.amr@outlook.com>
2019-03-03 00:51:41 +0200

    Initialise

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,112 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-navigation-drawer
+      app
+      id="app-drawer"
+      floating
+      persistent
+      mobile-break-point="991"
+      width="320"
+    >
+      <v-list>
+        <v-list-group
+          v-for="item in menuItems"
+          :key="item.title"
+          v-model="item.active"
+          no-action
+        >
+          <template v-slot:activator>
+            <v-list-tile avatar :to="item.path">
+              <v-list-tile-avatar>
+                <v-icon :class="[item.iconClass]">{{ item.icon }}</v-icon>
+              </v-list-tile-avatar>
+              <v-list-tile-content>
+                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </template>
+
+          <v-list-tile v-for="subItem in item.items" :key="subItem.title" :to="subItem.path">
+            <v-list-tile-content>
+              <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
+              <v-list-tile-sub-title>{{ subItem.subtitle }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+
+            <v-list-tile-action>
+              <v-icon>{{ subItem.icon }}</v-icon>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      menuItems: [
+        {
+          icon: "dashboard",
+          title: "DashBoard",
+          subtitle: "Contain all info and stats",
+          active: true,
+          path: "/"
+        },
+        {
+          icon: "account_circle",
+          title: "Profile",
+          subtitle: "Show Your Profile",
+          path: "/profile"
+        },
+        {
+          icon: "people",
+          title: "Customers",
+          subtitle: "Shows All Customers",
+          path: "/customers",
+          items: [
+            {
+              icon: "add_circle",
+              title: "Add",
+              subtitle: "Add A New Customer",
+              path: "/customers/add"
+            },
+            {
+              icon: "remove_circle",
+              title: "Remove",
+              subtitle: "Remove Customer",
+              path: "/customers/remove"
+            }
+          ]
+        },
+        {
+          icon: "work",
+          title: "Staff",
+          subtitle: "Shows All Staff Members",
+          path: "/staff",
+          items: [
+            {
+              icon: "add_circle",
+              title: "Add",
+              subtitle: "Add A New Staff",
+              path: "/staff/add"
+            },
+            {
+              icon: "remove_circle",
+              title: "Remove",
+              subtitle: "Remove Staff",
+              path: "/staff/remove"
+            }
+          ]
+        }
+      ]
+    };
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
+import vuetify from './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
 
 new Vue({
+  vuetify,
   router,
   store,
   render: h => h(App)
