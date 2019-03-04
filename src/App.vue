9d2f1f5ea617a591914428e256e2dcb35ec8b55c
Mohamed Amr Mahdy <dev.mohamed.amr@outlook.com>
2019-03-04 17:59:15 +0200

    navigation style upgrade

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,28 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      app
-      id="app-drawer"
-      floating
-      persistent
-      mobile-break-point="991"
-      width="320"
-    >
-      <v-list>
-        <v-list-group
-          v-for="item in menuItems"
-          :key="item.title"
-          v-model="item.active"
-          no-action
-        >
-          <template v-slot:activator>
-            <v-list-tile avatar :to="item.path">
-              <v-list-tile-avatar>
-                <v-icon :class="[item.iconClass]">{{ item.icon }}</v-icon>
-              </v-list-tile-avatar>
+    <v-navigation-drawer dark app fixed width="260" class="elevation-8">
+      <v-toolbar color="primary elevation-5" style="border-radius: 0px 0px 0px 30px;">
+        <v-avatar>
+          <v-icon dark>fitness_center</v-icon>
+        </v-avatar>
+        <v-toolbar-title>
+          <span class="text-uppercase">Gym System</span>
+        </v-toolbar-title>
+      </v-toolbar>
+      <v-list expand>
+        <template v-for="item in menuItems">
+            <v-subheader v-if="item.header" :key="item.title" class="text-uppercase font-weight-light">{{ item.header }}</v-subheader>
+            <v-divider v-else-if="item.divider" :key="item.title"></v-divider>
+            <v-list-tile v-else :key="item.title" :to="item.path" ripple="ripple" >
+              <v-list-tile-action>
+                <v-icon medium>{{ item.icon }}</v-icon>
+              </v-list-tile-action>
               <v-list-tile-content>
-                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-                <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
+                <v-list-tile-title class="text-uppercase">{{ item.title }}</v-list-tile-title>
+                <v-list-tile-sub-title class="caption text-capitalize">{{ item.subtitle }}</v-list-tile-sub-title>
               </v-list-tile-content>
             </v-list-tile>
-          </template>
-
-          <v-list-tile v-for="subItem in item.items" :key="subItem.title" :to="subItem.path">
-            <v-list-tile-content>
-              <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
-              <v-list-tile-sub-title>{{ subItem.subtitle }}</v-list-tile-sub-title>
-            </v-list-tile-content>
-
-            <v-list-tile-action>
-              <v-icon>{{ subItem.icon }}</v-icon>
-            </v-list-tile-action>
-          </v-list-tile>
-        </v-list-group>
+        </template>
       </v-list>
     </v-navigation-drawer>
     <v-content>
@@ -55,44 +40,69 @@ export default {
         {
           icon: "dashboard",
           title: "DashBoard",
-          subtitle: "Contain all info and stats",
-          active: true,
+          subtitle: "contain all info and stats",
           path: "/"
         },
         {
           icon: "account_circle",
           title: "Profile",
-          subtitle: "Show Your Profile",
+          subtitle: "show Your Profile",
           path: "/profile"
+        },
+        {
+          divider: true
+        },
+        {
+          header: "Main Control"
         },
         {
           icon: "people",
           title: "Customers",
-          subtitle: "Shows All Customers",
-          path: "/customers",
-          items: [
-            {
-              icon: "add_circle",
-              title: "Add",
-              subtitle: "Add A New Customer",
-              path: { name: 'customers', params: { addUser: true }}
-            }
-          ]
+          subtitle: "shows All Customers",
+          path: "/customers"
+        },
+        {
+          icon: "rss_feed",
+          title: "subscriptions",
+          subtitle: "shows subscriptions types",
+          path: "/subscriptions"
         },
         {
           icon: "work",
           title: "Staff",
           subtitle: "Shows All Staff Members",
-          path: "/staff",
-          items: [
-            {
-              icon: "add_circle",
-              title: "Add",
-              subtitle: "Add A New Staff",
-              path: { name: 'staff', params: { addUser: true }}
-            }
-          ]
-        }
+          path: "/staff"
+        },
+        {
+          divider: true
+        },
+        {
+          header: "Others"
+        },
+        {
+          icon: "library_books",
+          title: "Documentation",
+          subtitle: "",
+          path: "/documentation"
+        },
+        {
+          icon: "help",
+          title: "Contact Us",
+          subtitle: "",
+          path: "/contact"
+        },
+        {
+          icon: "info",
+          title: "About System",
+          subtitle: "",
+          path: "/info"
+        },
+        {
+          icon: "tune",
+          title: "Settings",
+          subtitle: "",
+          path: "/settings"
+        },
       ]
     };
   }
