d46bbec590eb04788d786db595afa63b99f85833
Mohamed Amr Mahdy <dev.mohamed.amr@outlook.com>
2019-03-05 13:55:59 +0200

    remove helloworld

--- src/views/DashBoard.vue
+++ src/views/DashBoard.vue
@@ -1,17 +1,7 @@
 <template>
 <v-app>
   <v-container>
-    <HelloWorld />
+
   </v-container>
 </v-app>
 </template>
-
-<script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    components: {
-      HelloWorld
-    }
-  }
-</script>
