b3c72c76b1d07473f23266aa01998dbf8d44baf0
Mohamed Amr Mahdy <dev.mohamed.amr@outlook.com>
2019-05-02 01:09:01 +0200

    Fix Dashboard

--- src/store/modules/apiDashboard.js
+++ src/store/modules/apiDashboard.js
@@ -14,7 +14,27 @@ export default {
       state.status = "loading";
     },
     addCashier_success(state, data) {
-      state.dashboardData = data;
+      let myObj = {};
+      myObj.dayCheckins = {};
+      myObj.monthlyEarnings = {};
+      myObj.subscriptions = {};
+      myObj.totalCustomers = data.totalCustomers;
+      myObj.totalCheckinsToday = data.totalCheckinsToday;
+      myObj.earningsToday = data.earningsToday;
+      myObj.totalEarnings = data.totalEarnings;
+      data.dayCheckins.forEach((e)=>{
+        myObj.dayCheckins[e.day] = e.totalCheckins;
+        })
+      data.monthlyEarnings.forEach((e)=>{
+        myObj.monthlyEarnings[e.month] = e.amount;
+        })
+      data.subscriptions.forEach((e)=>{
+        myObj.subscriptions[e.name] = e.totalSubscribers;
+        })
+      JSON.stringify(myObj.dayCheckins);
+      JSON.stringify(myObj.monthlyEarnings);
+      JSON.stringify(myObj.subscriptions);
+      state.dashboardData = myObj;
       state.status = "success";
     },
     addCashier_error(state) {
